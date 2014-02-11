da35987f34ba585358cffe7f791c43cc9faec059
kcecireyes <kcecireyes@gmail.com>
2014-02-11 18:27:55 -0500

    First commit, added README and app

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ rcoms3101
 =========
 
 Repo for Rails course
+ - End Product: a small CMS to manage interactive content 
---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '32ec5690c8c36aff4ef87e9c5bbdb0692a5d26e71f770a38689850fb9284dae68bb23c18bfd1de026dd921f2c6847bc1deef5d7103b69850c27f27dd6947dc8b'
